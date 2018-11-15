4f1117b657c0d8380e323eae2b6a3e195cfa0237
JDowd <jamesddowd@outlook.com>
2018-11-14 23:23:13 -0800

    created file directory

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burger (burger_name, devoured) VALUES ('Dicks Drive-In', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('In-N-Out', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Red Mill', false);
