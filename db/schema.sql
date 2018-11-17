2ffacd5802ae541a8750eea5e2394dba80ee0fae
JDowd <jamesddowd@outlook.com>
2018-11-16 23:46:52 -0800

    added burgers_controller + handlebars

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,7 @@
-DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE burger
+CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burger (burger_name, devoured) VALUES ('Dicks Drive-In', true);
-INSERT INTO burger (burger_name, devoured) VALUES ('In-N-Out', true);
-INSERT INTO burger (burger_name, devoured) VALUES ('Red Mill', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Dicks Drive-In', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('In-N-Out', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Red Mill', false);
